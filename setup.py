4f4b5f234066f3be9921822054ebffca16f10b0c
csterling <coreytsterling@gmail.com>
2020-11-03 13:06:18 +1300

    Removed direct wai.annotations dependency, instead opting for indirect dependency via ufdl-annotation-utils.

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
     author='Corey Sterling',
     author_email='coreytsterling@gmail.com',
     install_requires=[
-        "wai.annotations==0.4.6",
         "ufdl.pythonclient==0.0.1",
         "ufdl.json-messages==0.0.1",
         "ufdl-annotation-utils==0.0.1"
