1e5f9f9218cd9caac220cc58e1dd089fb04faa66
csterling <coreytsterling@gmail.com>
2022-09-13 10:20:45 +1200

    Added ability to specify timestamps of unlabelled frames to extract from videos.

--- src/ufdl/annotations_plugin/image/object_detection/component/_UFDLImageObjectDetectionReader.py
+++ src/ufdl/annotations_plugin/image/object_detection/component/_UFDLImageObjectDetectionReader.py
@@ -1,12 +1,15 @@
 import os
+import re
 import tempfile
-from typing import Iterator, List, Tuple
+from abc import ABC, abstractmethod
+from fnmatch import fnmatchcase
+from fractions import Fraction
+from typing import Dict, Iterator, List, Optional, Tuple
 
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from ufdl.json.object_detection import (
     Annotation,
     ImageAnnotation,
-    VideoAnnotation,
     Image as JSONImage,
     Video as JSONVideo
 )
@@ -19,6 +22,8 @@ from wai.annotations.domain.image.object_detection import ImageObjectDetectionIn
 from wai.annotations.domain.image.object_detection.util import set_object_label, set_object_prefix
 
 from wai.common.adams.imaging.locateobjects import LocatedObjects, LocatedObject
+from wai.common.cli import CLIRepresentable
+from wai.common.cli.options import TypedOption
 
 from wai.json.object import Absent
 from wai.json.raw import RawJSONArray, RawJSONObject
@@ -26,10 +31,139 @@ from wai.json.raw import RawJSONArray, RawJSONObject
 from ....common.component import UFDLReader
 
 
+class UnlabelledExtractionSpecHolder(CLIRepresentable):
+    def cli_repr(self) -> str:
+        return self._repr
+
+    @classmethod
+    def from_cli_repr(cls, cli_string: str) -> 'UnlabelledExtractionSpecHolder':
+        return UnlabelledExtractionSpecHolder(cli_string)
+
+    def __init__(self, repr: str):
+        self._repr = repr
+        self._spec = UnlabelledExtractionSpec.from_string(repr)
+
+    @property
+    def spec(self):
+        return self._spec
+
+
+class UnlabelledExtractionSpec(ABC):
+    # Fraction parsing is implemented by the Fraction constructor, hence the .* matchers
+    STRING_SPEC_REGEX = re.compile(
+        r"""
+        ^                                               # Regex start
+        ((?P<filename_glob>.*)@)?                         # Optional glob pattern for matching filenames
+        (
+            (every:(?P<step>-?[^+-]*)(?P<offset>[+-].*)?) # An every step specifier with optional offset
+            |                                           # or,
+            (?P<times>[^,]*(,[^,]*)*)                   # a comma separated list of specific times
+        )
+        $                                               # Regex end
+        """,
+        flags=re.VERBOSE
+    )
+
+    def __init__(
+            self,
+            filename_glob_pattern: str
+    ):
+        self._filename_glob_pattern = filename_glob_pattern
+
+    def matches_filename(self, filename: str) -> bool:
+        return fnmatchcase(filename, self._filename_glob_pattern)
+
+    @staticmethod
+    def from_string(string: str) -> 'UnlabelledExtractionSpec':
+        match = UnlabelledExtractionSpec.STRING_SPEC_REGEX.match(string)
+        glob = match.group('filename_glob')
+        if glob is None:
+            glob = "*"
+        step = match.group('step')
+        if step is not None:
+            step = Fraction(step)
+            offset = match.group('offset')
+            if offset is None:
+                offset = Fraction()
+            else:
+                offset = Fraction(offset)
+            return EveryUnlabelledExtractionSpec(glob, step, offset)
+        else:
+            times = match.group('times').split(',')
+            return SpecificUnlabelledExtractionSpec(glob, *map(Fraction, times))
+
+    @abstractmethod
+    def get_times(self, length: float) -> Iterator[float]:
+        raise NotImplementedError(self.get_times.__qualname__)
+
+
+class EveryUnlabelledExtractionSpec(UnlabelledExtractionSpec):
+    def __init__(
+            self,
+            filename_glob_pattern: str,
+            step: Fraction,
+            offset: Fraction
+    ):
+        super().__init__(filename_glob_pattern)
+        if step == Fraction():
+            raise ValueError(f"Step can't be zero")
+        self._step = step
+        self._offset = offset
+
+    def get_times(self, length: float) -> Iterator[float]:
+        step = self._step
+        start = self._offset
+        if step < 0:
+            if start > 0:
+                start %= step
+            start += length
+        else:
+            if start < 0:
+                start %= 0
+
+        class It(Iterator[float]):
+            def __next__(self) -> float:
+                nonlocal step, start, length
+                result = start
+                if result > length or result < 0:
+                    raise StopIteration
+                start += step
+                return float(result)
+
+        return It()
+
+
+class SpecificUnlabelledExtractionSpec(UnlabelledExtractionSpec):
+    def __init__(
+            self,
+            filename_glob_pattern: str,
+            *times: Fraction
+    ):
+        super().__init__(filename_glob_pattern)
+        self._times = list(times)
+
+    def get_times(self, length: float) -> Iterator[float]:
+        yield from (
+            float(time)
+            for time in self._times
+            if 0 <= time <= length
+        )
+
+
 class UFDLImageObjectDetectionReader(UFDLReader[ImageObjectDetectionInstance]):
     """
     Reader which reads object-detection annotations from a UFDL server.
     """
+    # Specification of unlabelled frames to extract from videos
+    extract_unlabelled: Optional[List[UnlabelledExtractionSpecHolder]] = TypedOption(
+        "--unlabelled",
+        type=UnlabelledExtractionSpecHolder,
+        nargs="+",
+        required=False,
+        help="the list of datasets to convert (omit version for latest)",
+        metavar="[GLOB@](every:STEP[+/-OFFSET] | TIME[,TIME]*"
+    )
+
     def read_annotations(
             self,
             pk: int,
@@ -127,8 +261,8 @@ class UFDLImageObjectDetectionReader(UFDLReader[ImageObjectDetectionInstance]):
             json_image.annotations
         )
 
-    @staticmethod
     def get_video_frame_instances(
+            self,
             filename: str,
             file_data: bytes,
             file_type: RawJSONObject,
@@ -149,13 +283,38 @@ class UFDLImageObjectDetectionReader(UFDLReader[ImageObjectDetectionInstance]):
             else None
         )
 
-        # Get the annotations and sort them by frame-time
-        video_annotations: List[VideoAnnotation] = list(json_video.annotations)
-        video_annotations.sort(key=lambda video_annotation: video_annotation.time)
+        # Get the unlabelled extraction specifiers that correspond to this video
+        unlabelled_extractors = [
+            extractor
+            for extractor in self.extract_unlabelled
+            if extractor.spec.matches_filename(filename)
+        ] if self.extract_unlabelled is not None else []
 
-        # If there are no annotated frames, skip this video
-        if len(video_annotations) == 0:
+        # Create a set of timestamps to extract (of unlabelled frames)
+        extraction_times = set(
+            time
+            for extractor in unlabelled_extractors
+            for time in extractor.spec.get_times(json_video.length)
+        )
+
+        # Add the labelled frame-times as well
+        extraction_times.update(
+            annotation.time
+            for annotation in json_video.annotations
+        )
+
+        # If there are no frames to extract, skip this video
+        if len(extraction_times) == 0:
             return
+
+        # Group the annotations by frame-time (as image-annotations)
+        frame_annotations: Dict[float, List[ImageAnnotation]] = {}
+        for annotation in json_video.annotations:
+            frame_annotation_list = frame_annotations.get(annotation.time)
+            if frame_annotation_list is None:
+                frame_annotation_list = []
+                frame_annotations[annotation.time] = frame_annotation_list
+            frame_annotation_list.append(annotation.to_image_annotation())
 
         # We need to write the video data to disk so that FFMPEG can read it,
         # so do so in a temporary file
@@ -170,15 +329,7 @@ class UFDLImageObjectDetectionReader(UFDLReader[ImageObjectDetectionInstance]):
             # Open the video with FFMPEG
             with VideoFileClip(video_filename, audio=False) as video_clip:
                 # Create images
-                while len(video_annotations) > 0:
-                    # Get the frame-time we are creating an image for
-                    frame_time = video_annotations[0].time
-
-                    # Convert the video annotations for this frame to image annotations
-                    image_annotations: List[ImageAnnotation] = []
-                    while len(video_annotations) > 0 and video_annotations[0].time == frame_time:
-                        image_annotations.append(video_annotations.pop(0).to_image_annotation())
-
+                for frame_time in extraction_times:
                     # Create an augmented filename for this frame of the video
                     augmented_filename = f"{filename}@|frametime={frame_time}|.jpg"
 
@@ -200,4 +351,4 @@ class UFDLImageObjectDetectionReader(UFDLReader[ImageObjectDetectionInstance]):
                         dimensions
                     )
 
-                    yield image, image_annotations
+                    yield image, frame_annotations.get(frame_time, [])
